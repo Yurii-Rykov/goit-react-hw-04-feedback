ea5cc3fa32476f71c113897b45b9578b6f6a0391
Yurii-Rykov <103988434+Yurii-Rykov@users.noreply.github.com>
2022-08-16 11:48:38 +0300

    hooks

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -2,24 +2,17 @@ import React from "react";
 import s from './Statistics.module.css'
 import PropTypes from 'prop-types'
 
-class Statistics extends React.Component {
-    
-    render() {
-        const { onStateGood, onStateNeutral, onStateBad, onTotal, onPositiv } = this.props
-        
-        return (
-     
+const Statistics = ({ onStateGood, onStateNeutral, onStateBad, onTotal, onPositiv }) => {
+
+    return(     
         <ul className={s.statistic_wrapper}>
           <li className={s.statistic_item}>Good: {onStateGood}</li>
           <li className={s.statistic_item}>Neutral: {onStateNeutral}</li>
           <li className={s.statistic_item}>Bad: {onStateBad}</li>
           <li className={s.statistic_item}>Total: {onTotal}</li>
           <li className={s.statistic_item}>PositiveFeedBack: {onPositiv.toFixed()}%</li>
-        </ul>
-       
-        )
-    }
-    
+        </ul>          
+)    
 }
 
 Statistics.propTypes = {
--- src/components/feedbackOptions/FeedbackOptions.js
+++ src/components/feedbackOptions/FeedbackOptions.js
@@ -2,20 +2,18 @@ import React from "react";
 import s from './FeedbackOptions.module.css'
 import PropTypes from 'prop-types'
 
-class FeedbackOptions extends React.Component{
-    constructor({ options, onLeaveFeedback }){
-        super()
-        this.btns = options.map( btn =>  
+const FeedbackOptions = ({options, onLeaveFeedback}) => {
+
+  function  renderBtns() {
+        return options.map( btn =>  
             <button key={btn} type="button" className={s.feedback_btn} onClick={() =>{onLeaveFeedback(btn)}}>{btn}</button>
         )
     }
-    render() {
         return (
             <div className={s.feedback_wrapper}> 
-            { this.btns}
+            {renderBtns()}
             </div>
         )
-    }
 }
 FeedbackOptions.propTypes = {
     options: PropTypes.arrayOf(PropTypes.oneOf(['good', 'neutral', 'bad'])),
