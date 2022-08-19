df26ea6a81f2496fbe26f90880b71a634d9c6201
Yurii-Rykov <103988434+Yurii-Rykov@users.noreply.github.com>
2022-08-19 13:40:46 +0300

    corrections

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,48 +1,47 @@
-import React from "react";
-import FeedbackOptions from "./feedbackOptions/FeedbackOptions";
-import Statistics from "./Statistics/Statistics"
-import Sections from "./Sections/Sections"
-import Notification from "./Notification/Notification"
+import React from 'react';
+import FeedbackOptions from './feedbackOptions/FeedbackOptions';
+import Statistics from './Statistics/Statistics';
+import Sections from './Sections/Sections';
+import Notification from './Notification/Notification';
+import { useState } from 'react';
 
-class App extends React.Component {
+const App = () => {
+  const [options, setOptions] = useState({ good: 0, neutral: 0, bad: 0 });
 
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  }
+  const increment = button => {
+    setOptions(prevState => ({
+      ...prevState,
+      [button]: prevState[button] + 1,
+    }));
+  };
 
-  increment = (button) => {
-    this.setState(prevState => ({
-      [button]: prevState[button] + 1
-    }))
-  }
+  const total = options.good + options.neutral + options.bad;
+  const positiv = (options.good / total) * 100;
 
-  render () {
-    const total = this.state.good + this.state.neutral + this.state.bad;
-    const positiv = this.state.good / total * 100;
-
-   return (
-   <div className="container">
-        
+  return (
+    <div className="container">
       <Sections title={'Please leave feedback'}>
-        <FeedbackOptions 
+        <FeedbackOptions
           options={['good', 'neutral', 'bad']}
-          onLeaveFeedback={this.increment}/>
-       </Sections>
+          onLeaveFeedback={increment}
+        />
+      </Sections>
 
-
-       <Sections title={'Statistics'}>
-        {total !== 0  
-        ?
-        <Statistics onStateGood={this.state.good} onStateNeutral={this.state.neutral} 
-        onStateBad={this.state.bad} onTotal={total} onPositiv={positiv}/>
-        :
-         <Notification/>}
-       </Sections> 
-
+      <Sections title={'Statistics'}>
+        {total !== 0 ? (
+          <Statistics
+            onStateGood={options.good}
+            onStateNeutral={options.neutral}
+            onStateBad={options.bad}
+            onTotal={total}
+            onPositiv={positiv}
+          />
+        ) : (
+          <Notification />
+        )}
+      </Sections>
     </div>
-    )
-}};
+  );
+};
 
 export default App;
